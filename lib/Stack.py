d9da45ab607a34ac6969768311b105c8dfb189b8
ombito <alvinombito09@gmail.com>
2023-10-05 17:50:57 +0300

    Complete assignment

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,36 @@
 class Stack:
 
     def __init__(self, items = [], limit = 100):
-        pass
+        self.items = items
+        self.limit = limit
 
     def isEmpty(self):
-        pass
+        return len(self.items) == 0
 
     def push(self, item):
-        pass
+        if len(self.items) < self.limit:
+            self.items.append(item)
+        else:
+            return f'Stack is full'
+        return self.items
 
     def pop(self):
-        pass
+        if not self.isEmpty():
+            return self.items.pop()
+        else:
+            return None
 
     def peek(self):
-        pass
+        return self.items.peek()
     
     def size(self):
-        pass
+        return len(self.items)
 
     def full(self):
-        pass
+        return self.size() >= self.limit
 
     def search(self, target):
-        pass
+        for i in (self.items): 
+             if i == target: 
+                 return sorted(self.items , reverse=True).index(i) 
+        return -1
